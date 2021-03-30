9c7d1230b549545f9dcfbf28cadb3638d71ad326
Naveen Setty <naveenkumarpg@gmail.com>
2021-03-30 10:35:00 +0100

    simple i am rich app.

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Welcome to My app')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.black26,
+        ),
+      ),
+    ),
+  );
 }
