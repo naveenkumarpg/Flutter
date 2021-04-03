5c6945bd571dee75f910fdd1332befd31b502777
Naveen Setty <naveenkumarpg@gmail.com>
2021-04-03 12:40:42 +0100

    Completed MagicBall app.

--- magic_ball/lib/main.dart
+++ magic_ball/lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: BallApp(),
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask me anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+      ),
+    ),
+  );
+}
+
+class BallApp extends StatefulWidget {
+  @override
+  _BallAppState createState() => _BallAppState();
+}
+
+class _BallAppState extends State<BallApp> {
+  int ballNumber = 1;
+
+  void answerBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            answerBall();
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
       ),
     );
+  }
+}
