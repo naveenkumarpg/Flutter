f4a4598a475a392991a36e5f3e55d04c4222107c
Naveen Setty <naveenkumarpg@gmail.com>
2021-04-08 08:25:59 +0100

    Navigator flutter in example.

---
+++ navigation_Flutter/lib/screen1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Screen 1'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.red,
+          child: Text('Go Forwards To Screen 2'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
+      ),
+    );
+  }
+}
